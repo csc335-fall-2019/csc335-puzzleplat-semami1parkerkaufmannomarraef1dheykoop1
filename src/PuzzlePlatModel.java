aa9f3098450a31fc2ce95aca625c345f3df90972
semami1 <semami@email.arizona.edu>
2019-11-21 00:38:18 -0700

    done with most of the level. Still needs rain to react to floor.

--- src/PuzzlePlatModel.java
+++ src/PuzzlePlatModel.java
@@ -1,4 +1,60 @@
+import java.util.ArrayList;
+import java.util.Observable;
 
-public class PuzzlePlatModel {
+import javafx.scene.shape.Shape;
+
+public class PuzzlePlatModel extends Observable{
+	ArrayList<Shape> floors = new ArrayList<>();
+	ArrayList<Shape> obstacles = new ArrayList<>();
+	//Character player1
+	
+	/**
+	 * getter for list of floors
+	 * @return list of floors
+	 */
+	public ArrayList<Shape> getFloors() {
+		return floors;
+	}
+	
+	/**
+	 * getter for list of obstacles
+	 * @return list of obstacles
+	 */
+	public ArrayList<Shape> getObstacles() {
+		return obstacles;
+	}
+	
+	/**
+	 * add a floor to the list of floors
+	 * @param floor to be added
+	 */
+	public void addFloor(Shape floor) {
+		floors.add(floor);
+	}
+	
+	/**
+	 * add a list of obstacles to the list of obstacles
+	 * @param obstacle to be added
+	 */
+	public void addObstacle(Shape obstacle) {
+		obstacles.add(obstacle);
+	}
+	
+	/**
+	 * tells model that changes have been made.
+	 */
+	public void update() {
+		setChanged();
+	}
+	
+	/**
+	 * set new obstacles. this is mostly used for moving rain.
+	 * @param newObstacles list of new objects.
+	 */
+	public void setAllObstacles(ArrayList<Shape> newObstacles) {
+		obstacles = newObstacles;
+	}
+	
+	
 
 }
---
+++ src/StageOneGUI.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.Observable;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+
+public class StageOneGUI extends Application implements java.util.Observer{
+	
+	PuzzlePlatController controller = new PuzzlePlatController();
+	GraphicsContext gc;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		controller.start();//starts game clock
+		controller.addObserver(this);
+		
+		primaryStage.setTitle("Tutorial Stage");
+		Canvas canvas = new Canvas(1200,300);
+		gc = canvas.getGraphicsContext2D();
+		
+		gc.setFill(Color.LIGHTSKYBLUE);
+		gc.fillRect(0, 0, 1200, 300);
+	
+		controller.makeStageOneFloors();//sets up level
+		drawShapes(controller.getFloors());
+		
+		controller.makeRain(1, 1);//makes rain
+		controller.makeStageOneObstacles();
+		drawShapes(controller.getObstacles());
+		
+		Group root = new Group();
+		root.getChildren().add(canvas);
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		
+	}
+	/**
+	 * draws all shapes on the GUI that are saved in the state of the game
+	 * @param shapes list of shapes to be drawn
+	 * @param gc graphicContext being drawn on
+	 */
+	public void drawShapes(ArrayList<Shape> shapes) {
+		for(Shape shape: shapes) {
+			if(shape instanceof Rectangle) {
+				gc.setFill(((Rectangle) shape).getFill());
+				gc.fillRect(((Rectangle) shape).getX(), ((Rectangle) shape).getY(), 
+						((Rectangle) shape).getWidth(), ((Rectangle) shape).getHeight());
+
+			}else if (shape instanceof myCircle) {
+				gc.setFill(((myCircle) shape).getFill());
+				gc.fillOval(((myCircle) shape).getCenterX(), ((myCircle) shape).getCenterY(),
+						((myCircle) shape).getRadius(), ((myCircle) shape).getRadius());;
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * updates view
+	 * 
+	 * @param o observable
+	 * @param arg a list containing two lists. the first index is a list of all floors.
+	 * 			the second list is a list of all obstacles.
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void update(Observable o, Object arg) {
+		gc.clearRect(0, 0, 1200, 300);
+		gc.setFill(Color.LIGHTSKYBLUE);
+		gc.fillRect(0, 0, 1200, 300);
+		if(controller.getObstacles().size() < 50) {
+			controller.makeRain(1, 1);
+		}
+		drawShapes(((ArrayList<Shape>)((ArrayList<Object>)arg).get(0)));
+		drawShapes(((ArrayList<Shape>)((ArrayList<Object>)arg).get(1)));
+		
+		
+		
+	}
+
+}
