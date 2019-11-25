f6fa4420d3b5c9c68347f34cc5e3f6ffb0ce772e
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2019-11-24 20:28:16 -0700

    JavaFX Start Menu

--- src/PuzzlePlat.java
+++ src/PuzzlePlat.java
@@ -1,8 +1,8 @@
+import java.util.Arrays;
+
 import javafx.application.Application;
 
 public class PuzzlePlat {
 
-	public static void main(String args[]) {
-		Application.launch(StageOneGUI.class, args);
-	}
+
 }
