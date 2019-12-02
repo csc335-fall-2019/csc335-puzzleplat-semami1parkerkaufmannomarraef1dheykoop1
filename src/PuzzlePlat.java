b550d9029649c62f98a0dff0f51b3478e5544719
dawsonheykoop <dawsonheykoop@dawsons-mbp.localdomain>
2019-12-02 14:45:30 -0700

    main

--- src/PuzzlePlat.java
+++ src/PuzzlePlat.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 import javafx.application.Application;
 
 public class PuzzlePlat {
-
+	public static void main(String[] args) {
+		Application.launch(StageOneGUI.class);
+		
+	}
 
 }
